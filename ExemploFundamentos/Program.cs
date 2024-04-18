ea66688961413ae897c69162e03b22caaac55097
Ramon <ramon.palheta.pessoa@hotmail.com>
2024-04-18 17:09:11 -0400

    Tipos de variáveis

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,6 +1,22 @@
 ﻿using ExemploFundamentos.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome="Pessoa";
-p1.Idade=44;
-p1.Apresentar();
+
+string apresentacao = "Hello, welcome";
+
+int quantidade = 5;
+double altura = 1.94;
+
+decimal preco = 1.60M;
+bool condicao = true;
+
+Console.WriteLine( apresentacao);
+Console.WriteLine("Quantidade:" + quantidade);
+Console.WriteLine("Altura: " + altura.ToString("0.00"));
+Console.WriteLine("Preço: " + preco);
+Console.WriteLine("Condição: " + condicao);
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome="Pessoa";
+// p1.Idade=44;
+// p1.Apresentar();
