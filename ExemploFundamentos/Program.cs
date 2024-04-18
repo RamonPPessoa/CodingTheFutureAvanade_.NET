105165516dac14b989e01c0a19a88557a566d1a2
Ramon <ramon.palheta.pessoa@hotmail.com>
2024-04-18 17:21:29 -0400

    Trabalhando com o tipo DateTime

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,19 +1,32 @@
 ﻿using ExemploFundamentos.Models;
 
 
-string apresentacao = "Hello, welcome";
 
-int quantidade = 5;
-double altura = 1.94;
 
-decimal preco = 1.60M;
-bool condicao = true;
+DateTime dataAtual = DateTime.Now;
 
-Console.WriteLine( apresentacao);
-Console.WriteLine("Quantidade:" + quantidade);
-Console.WriteLine("Altura: " + altura.ToString("0.00"));
-Console.WriteLine("Preço: " + preco);
-Console.WriteLine("Condição: " + condicao);
+System.Console.WriteLine(dataAtual);
+
+DateTime dataAtua2 = DateTime.Now.AddDays(2);
+System.Console.WriteLine(dataAtua2);
+System.Console.WriteLine(dataAtua2.ToString("dd/MM/yyyy HH:mmgit commit "));
+
+
+
+
+// string apresentacao = "Hello, welcome";
+
+// int quantidade = 5;
+// double altura = 1.94;
+
+// decimal preco = 1.60M;
+// bool condicao = true;
+
+// Console.WriteLine( apresentacao);
+// Console.WriteLine("Quantidade:" + quantidade);
+// Console.WriteLine("Altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Preço: " + preco);
+// Console.WriteLine("Condição: " + condicao);
 
 
 // Pessoa p1 = new Pessoa();
